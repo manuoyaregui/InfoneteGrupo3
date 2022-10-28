f250c83f004d9cc4da6b8851a730fb0781a893be
Manuel Oyaregui <manu.oyaregui@gmail.com>
2022-10-27 21:43:29 -0300

    Ahora los roles tienen 'Poder'

--- helper/Render.php
+++ helper/Render.php
@@ -12,13 +12,48 @@ class Render{
     }
 
     public function render($contentFile , $data = array() ){
+        $data = array_merge( $data,$this->manejarDatosDeSesion() );
+
         $contentAsString =  file_get_contents($contentFile);
-        if( isset($_SESSION['usuario']) && !empty($_SESSION['usuario']) ){
-            $data['isLogged'] = true;
-        }
-        else{
-            $data['isLogged'] = false;
-        }
         return  $this->mustache->render($contentAsString, $data);
+    }
+
+    public function redirect($destination){
+        header("location: ".$destination);
+        exit();
+    }
+
+    private function estaElUsuarioLogeado():bool{
+        return  isset($_SESSION['usuario']) &&
+                !empty($_SESSION['usuario']);
+    }
+
+    private function manejarDatosDeSesion():array{
+        $data = array();
+
+        $data['isLogged'] = $this->estaElUsuarioLogeado();
+
+        if($data['isLogged']){
+            //Roles
+            if (isset($_SESSION['rol'])) {
+                $rolUsuario = $_SESSION['rol']['nombre'];
+                $data['rol'] = $rolUsuario;
+                switch ($rolUsuario) {
+                    case 'ADMINISTRADOR':
+                        $data['esAdmin'] = true;
+                        $data['esEscritor'] = true;
+                        $data['esLector'] = true;
+                        break;
+                    case 'ESCRITOR':
+                        $data['esEscritor'] = true;
+                        $data['esLector'] = true;
+                        break;
+                    case 'LECTOR':
+                        $data['esLector'] = true;
+                        break;
+                }
+            }
+        }
+        return $data;
     }
 }
