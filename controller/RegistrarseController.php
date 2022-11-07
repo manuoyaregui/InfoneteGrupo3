beadebb935823ac51c19c87ed7a840d4f2e4ce79
AlanForno <aforno@alumno.unlam.edu.ar>
2022-11-07 17:57:55 -0300

    Envio de mail con email y hash para activacion

--- controller/RegistrarseController.php
+++ controller/RegistrarseController.php
@@ -4,11 +4,13 @@ class RegistrarseController
 {
     private $render;
     private $registrarseModel;
+    private $mailer;
 
-    public function __construct($registrarseModel,$render)
+    public function __construct($registrarseModel, $render, $mailer)
     {
         $this->registrarseModel = $registrarseModel;
         $this->render = $render;
+        $this->mailer = $mailer;
     }
 
     public function execute()
@@ -22,14 +24,20 @@ class RegistrarseController
         $passwordMD5 = md5($_POST["password"]);
         $latitud = $_POST["latitud"];
         $longitud = $_POST["longitud"];
-        $rol = 1 ;
+        $rol = 1;
+        $hash = md5(time());
 
         if (!empty($nombre) && !empty($email) && !empty($passwordMD5) && !empty($latitud) && !empty($longitud) && !empty($rol)) {
-            $resultado = $this->registrarseModel->procesarFormularioRegistarseLector($nombre, $email, $passwordMD5, $latitud, $longitud, $rol);
+            $resultado = $this->registrarseModel->procesarFormularioRegistarseLector($nombre, $email, $passwordMD5, $latitud, $longitud, $rol, $hash);
 
             if ($resultado){
-                $data["mensaje"] = "Registrado correctamente";
-                echo $this->render->render("view/login.mustache", $data);
+                $data["mensajeActivacion"] = "Se envio un enlace de activacion al mail " . $email;
+
+                $mensajeActivacion = "<h1>Haz click en el siguiente enlace para activar tu cuenta</h1>
+                                      <a href='http://localhost/registrarse/activarUsuario/email=$email&hash=$hash'>Activar cuenta</a>";
+                $this->mailer->enviarMail($email, "Activacion de cuenta en Infonete", $mensajeActivacion);
+
+                echo $this->render->render("view/registroView.mustache", $data);
             } else{
                 $data["mensaje"] = "Ese mail ya esta registrado";
                 echo $this->render->render("view/registroView.mustache", $data);
@@ -41,4 +49,16 @@ class RegistrarseController
         }
 
     }
+
+    public function activarUsuario() {
+        $email = $_GET["email"];
+        $hash = $_GET["hash"];
+
+        $resultado = $this->registrarseModel->activarUsuario($email, $hash);
+
+        if ($resultado) {
+            echo $this->render->render("login.mustache");
+        }
+    }
+
 }
