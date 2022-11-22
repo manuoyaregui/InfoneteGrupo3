5a0e2e0f1a973503c998f55a2740956ed2671b58
AlanForno <aforno@alumno.unlam.edu.ar>
2022-11-22 17:39:13 -0300

    Al registrar usuario por defecto esta inactivo

--- model/RegistrarseModel.php
+++ model/RegistrarseModel.php
@@ -23,7 +23,7 @@ class RegistrarseModel
 
     private function crearUsuario($nombre, $email, $password, $latitud, $longitud, $idRol, $hash){
         $sql = "INSERT INTO usuario (nombre, email, password, latitud, longitud, idRol, hashVerificacion, idEstado) 
-             VALUES ('".$nombre."','".$email."','".$password."','".$latitud."', '".$longitud."', '".$idRol."', '".$hash."', '2')";
+             VALUES ('".$nombre."','".$email."','".$password."','".$latitud."', '".$longitud."', '".$idRol."', '".$hash."', 1)";
         return $this->database->execute($sql);
     }
 
