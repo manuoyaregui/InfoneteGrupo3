2e71b76e5564e17160aaa45f0fdc07be291860b1
stitchees <54647336+stitchees@users.noreply.github.com>
2022-11-22 18:16:56 -0300

    boton vista lector

--- controller/ArticuloController.php
+++ controller/ArticuloController.php
@@ -21,22 +21,22 @@ class ArticuloController
         $idArticulo = $_GET["articulo"];
         $idProducto = $_GET["idProducto"];
         $idEdicion = $_GET["idEdicion"];
-        $idUsuario = $_SESSION["idUsuario"];
+        if (isset( $_SESSION["idUsuario"])) {
+            $idUsuario = $_SESSION["idUsuario"];
+            $data["articulo"] = $this->articuloModel->getArticuloYSeccionPorId($idArticulo);
 
-        $data["articulo"] = $this->articuloModel->getArticuloYSeccionPorId($idArticulo);
+            $usuarioSuscripto = $this->suscripcionYCompraModel->usuarioSuscripto($idUsuario, $idProducto);
+            $usuarioPoseeLaEdicion = $this->suscripcionYCompraModel->usuarioPoseeEdicion($idUsuario, $idEdicion);
 
-        $usuarioSuscripto = $this->suscripcionYCompraModel->usuarioSuscripto($idUsuario, $idProducto);
-        $usuarioPoseeLaEdicion = $this->suscripcionYCompraModel->usuarioPoseeEdicion($idUsuario, $idEdicion);
-
-        if ($usuarioSuscripto || $usuarioPoseeLaEdicion) {
-            $data["usuarioPuedeVerArticulo"] = true;
-            echo $this->render->render("view/articuloView.mustache", $data);
-        } else {
+            if ($usuarioSuscripto || $usuarioPoseeLaEdicion) {
+                $data["usuarioPuedeVerArticulo"] = true;
+                echo $this->render->render("view/articuloView.mustache", $data);
+            }
+        }else {
             // Poner $data["usuarioPuedeVerArticulo"] = false en el else es opcional, funciona igual
             $data["usuarioPuedeVerArticulo"] = false;
             echo $this->render->render("view/articuloView.mustache", $data);
         }
-
     }
 
 }
